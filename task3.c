5d6070bfea72750fc4682c31867560b991fea352
Kevin <kyuu0034@student.monash.edu>
2026-08-21 01:56:32 +1000

    task 3 done, completely change, now simply added a line of pragma for OpenMP due to task 1 being changed to basically fit task 3 easily

--- task3.c
+++ task3.c
@@ -1,65 +1,77 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
 #include <omp.h>
 
-int main() { 
+int main() {
+
     /* Get user input for n */
     int n;
     printf("Find all prime numbers strictly less than which number? \n");
     scanf("%d", &n);
 
-    FILE *fptr = NULL; /*By default set file pointer to NULL, it is only needed if n >= 100*/
+    struct timespec start, end, startComp, endComp;
+    double time_taken;
 
-    if (n>= 100){
+    /* Overall timer starts right after n is known. */
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    FILE *fptr = NULL; /* Only needed if n >= 100 */
+    if (n >= 100) {
         fptr = fopen("output.txt", "w");
     }
 
-    /* Print 2 first, as it is the only even prime */
-    if (n > 2) {
-        if (n<100){
-                printf("2 ");
-            }
-            else {
-                fprintf(fptr, "2 ");
-            }
-    }
+    /* prime[num] = 1 means num is prime */
+    int *prime = calloc(n, sizeof(int));
+    if (n > 2) prime[2] = 1;
 
-    /* Outer loop: Check every odd number from 3 up to but excluding n */
+    /* Computational timer includes only the search work */
+    clock_gettime(CLOCK_MONOTONIC, &startComp);
+
     #pragma omp parallel for schedule(dynamic, 1000)
     for (int num = 3; num < n; num += 2) {
         int is_prime = 1; /* Assume the number is prime */
         double limit = sqrt(num);
 
-        /* Inner loop: Test odd divisors */
         for (int i = 3; i <= limit; i += 2) {
             if (num % i == 0) {
                 is_prime = 0;
-                break;        /* Divisor found, stop checking */
+                break; /* Divisor found, stop checking */
             }
         }
+        prime[num] = is_prime;
+    }
 
-        /* If no divisors were found, print the number */
-        if (is_prime) {
-            #pragma omp critical
-            if (n<100){
+    clock_gettime(CLOCK_MONOTONIC, &endComp);
+    time_taken = (endComp.tv_sec - startComp.tv_sec) * 1e9;
+    time_taken = (time_taken + (endComp.tv_nsec - startComp.tv_nsec)) * 1e-9;
+    printf("Prime search complete - Computational time only(s): %lf\n", time_taken);
+
+    /* Write results - not part of the computational timer */
+    for (int num = 2; num < n; num++) {
+        if (prime[num]) {
+            if (n < 100) {
                 printf("%d ", num);
-            }
-            else {
+            } else {
                 fprintf(fptr, "%d ", num);
-    
             }
-            
         }
     }
-    
-    /* If n was greater than 100, close the txt file*/
+
+    if (n >= 100) fclose(fptr);
+    free(prime);
+
+    /* Overall timer stops after everything: setup, search, write and cleanup */
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    time_taken = (end.tv_sec - start.tv_sec) * 1e9;
+    time_taken = (time_taken + (end.tv_nsec - start.tv_nsec)) * 1e-9;
+
     if (n < 100) {
-        printf("\nCode finished executing.\n");
+        printf("\nOverall time (Including setup, search and write)(s): %lf\n", time_taken);
     } else {
-        printf("\nCode finished executing. Results saved to output.txt.\n");
-        fclose(fptr);
+        printf("\nOverall time (Including setup, search and write)(s): %lf. Results saved to output.txt.\n", time_taken);
     }
+
     return 0;
 }
